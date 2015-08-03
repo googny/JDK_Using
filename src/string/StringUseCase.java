838d1fee73a97b5693fe02268abfc8ebbe8b9c4e
googny <haoqit913@gmail.com>
2015-08-03 11:38:37 +0800

    String “==” 和equals() 方法，编译器优化等

--- src/string/StringUseCase.java
+++ src/string/StringUseCase.java
@@ -8,32 +8,56 @@ public class StringUseCase {
 
     public static void main(String[] args) {
 //        useEqualSign();
-        useEqualsMethod();
+//        useEqualsMethod();
+        JVMOptimize();
     }
 
     public static void useEqualSign() {
         //Test1;
         String aStr = "Hello World!";
         String bStr = "Hello World!";
-        assert aStr == bStr;
+        System.out.println(aStr == bStr);
 
         //Test2:编译器优化，将Hello+World合并成HelloWorld
         //因为“+”号左右都是常量，编译器认为不需要计算了
         String cStr = "Hello" + " World!";
         String dStr = "Hello World!";
-        assert cStr == dStr;
+        System.out.println(cStr == dStr);
     }
 
     public static void useEqualsMethod() {
         //Test1
         String aStr = "Hello World!";
         String bStr = "Hello World!";
-        assert aStr.equals(bStr);
+        System.out.println(aStr.equals(bStr));
 
         //Test2:编译器优化，将Hello+World合并成HelloWorld
         String cStr = "Hello" + " World!";
         String dStr = "Hello World!";
-        assert cStr.equals(dStr);
+        System.out.println(cStr.equals(dStr));
+    }
+
+    private static String getA() {
+        return "a";
+    }
+
+    public static void JVMOptimize() {
+        String a = "a";
+        String aa = new String("a");
+        System.out.println(a == aa); // false
+        final String c = "a";
+
+        String b = a + "b";
+        String d = c + "b";
+        String e = getA() + "b";
+        String f = aa + "b";
+
+        String compare = "ab";
+
+        System.out.println(b == compare); // false
+        System.out.println(d == compare); // true
+        System.out.println(e == compare); // false
+        System.out.println(f == compare); // false
     }
 }
 
